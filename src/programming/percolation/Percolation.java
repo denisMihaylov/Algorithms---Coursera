02aeea6c1b93846966177722466b5507818f783b
Mihaylov <denis.mihaylov@sap.com>
2017-08-15 14:44:19 +0300

    Changes indentation from tabs to spaces

--- src/programming/percolation/Percolation.java
+++ src/programming/percolation/Percolation.java
@@ -1,88 +1,89 @@
 package programming.percolation;
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-	private WeightedQuickUnionUF wquf;
+    private WeightedQuickUnionUF wquf;
 
-	private final int size;
-	private int openSites = 0;
-	private boolean[] open;
-	private boolean[] connectedToBottom;
-	private boolean percolates = false;
+    private final int size;
+    private int openSites = 0;
+    private boolean[] open;
+    private boolean[] connectedToBottom;
+    private boolean percolates = false;
 
-	public Percolation(int n) {
-		if (n < 1) {
-			throw new IllegalArgumentException();
-		}
-		size = n;
-		open = new boolean[n * n + 1];
-		wquf = new WeightedQuickUnionUF(n * n + 1);
-		connectedToBottom = new boolean[n * n + 1];
-		for (int i = 1; i <= size; i++) {
-			wquf.union(i, 0);
-			connectedToBottom[size * size + 1 - i] = true;
-		}
-	}
+    public Percolation(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }
+        size = n;
+        open = new boolean[n * n + 1];
+        wquf = new WeightedQuickUnionUF(n * n + 1);
+        connectedToBottom = new boolean[n * n + 1];
+        for (int i = 1; i <= size; i++) {
+            wquf.union(i, 0);
+            connectedToBottom[size * size + 1 - i] = true;
+        }
+    }
 
-	public void open(int row, int col) {
-		checkArguements(row, col);
-		if (!isOpen(row, col)) {
-			openSites++;
-			open[toIndex(row, col)] = true;
-			union(row, col, row - 1, col);
-			union(row, col, row + 1, col);
-			union(row, col, row, col - 1);
-			union(row, col, row, col + 1);
-			if (size == 1) {
-				percolates = true;
-			}
-		}
-	}
+    public void open(int row, int col) {
+        checkArguements(row, col);
+        if (!isOpen(row, col)) {
+            openSites++;
+            open[toIndex(row, col)] = true;
+            union(row, col, row - 1, col);
+            union(row, col, row + 1, col);
+            union(row, col, row, col - 1);
+            union(row, col, row, col + 1);
+            if (size == 1) {
+                percolates = true;
+            }
+        }
+    }
 
-	public boolean isOpen(int row, int col) {
-		checkArguements(row, col);
-		return open[toIndex(row, col)];
-	}
+    public boolean isOpen(int row, int col) {
+        checkArguements(row, col);
+        return open[toIndex(row, col)];
+    }
 
-	public boolean isFull(int row, int col) {
-		return isOpen(row, col) && wquf.connected(0, toIndex(row, col));
-	}
+    public boolean isFull(int row, int col) {
+        return isOpen(row, col) && wquf.connected(0, toIndex(row, col));
+    }
 
-	public int numberOfOpenSites() {
-		return openSites;
-	}
+    public int numberOfOpenSites() {
+        return openSites;
+    }
 
-	public boolean percolates() {
-		return percolates;
-	}
+    public boolean percolates() {
+        return percolates;
+    }
 
-	private void checkArguements(int row, int col) {
-		if (validateArguements(row, col)) {
-			throw new IllegalArgumentException();
-		}
-	}
+    private void checkArguements(int row, int col) {
+        if (validateArguements(row, col)) {
+            throw new IllegalArgumentException();
+        }
+    }
 
-	private boolean validateArguements(int row, int col) {
-		return row < 1 || row > size || col < 1 || col > size;
-	}
+    private boolean validateArguements(int row, int col) {
+        return row < 1 || row > size || col < 1 || col > size;
+    }
 
-	private int toIndex(int row, int col) {
-		return (row - 1) * size + col;
-	}
+    private int toIndex(int row, int col) {
+        return (row - 1) * size + col;
+    }
 
-	private void union(int row1, int col1, int row2, int col2) {
-		if (!validateArguements(row1, col1) && !validateArguements(row2, col2) && isOpen(row1, col1) && isOpen(row2, col2)) {
-			int root1 = wquf.find(toIndex(row1, col1));
-			int root2 = wquf.find(toIndex(row2, col2));
-			if (connectedToBottom[root1] && wquf.connected(0, root2) || connectedToBottom[root2] && wquf.connected(0, root1)) {
-				percolates = true;
-			}
-			if (connectedToBottom[root1] || connectedToBottom[root2]) {
-				connectedToBottom[root1] = connectedToBottom[root2] = true;
-			}
-			wquf.union(root1, root2);
-		}
-	}
+    private void union(int row1, int col1, int row2, int col2) {
+        if (!validateArguements(row1, col1) && !validateArguements(row2, col2) && isOpen(row1, col1) && isOpen(row2, col2)) {
+            int root1 = wquf.find(toIndex(row1, col1));
+            int root2 = wquf.find(toIndex(row2, col2));
+            if (connectedToBottom[root1] && wquf.connected(0, root2) || connectedToBottom[root2] && wquf.connected(0, root1)) {
+                percolates = true;
+            }
+            if (connectedToBottom[root1] || connectedToBottom[root2]) {
+                connectedToBottom[root1] = connectedToBottom[root2] = true;
+            }
+            wquf.union(root1, root2);
+        }
+    }
 
 }
--- src/programming/percolation/PercolationStats.java
+++ src/programming/percolation/PercolationStats.java
@@ -1,63 +1,64 @@
 package programming.percolation;
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-	private final double[] results;
-	
-	private Double mean;
-	private Double stddev;
+    private final double[] results;
 
-	// perform trials independent experiments on an n-by-n grid
-	public PercolationStats(int n, int trials) {
-		if (n <= 0 || trials <= 0) {
-			throw new IllegalArgumentException();
-		}
-		results = new double[trials];
-		for (int i = 0; i < trials; i++) {
-			Percolation perc = new Percolation(n);
-			while (!perc.percolates()) {
-				int row = StdRandom.uniform(n) + 1;
-				int col = StdRandom.uniform(n) + 1;
-				perc.open(row, col);
-			}
-			results[i] = (double)perc.numberOfOpenSites() / (n * n);
-		}
-	}
+    private Double mean;
+    private Double stddev;
 
-	// sample mean of percolation threshold
-	public double mean() {
-		if (mean == null) {
-			mean = StdStats.mean(results);
-		}
-		return mean;
-	}
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        results = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+            while (!perc.percolates()) {
+                int row = StdRandom.uniform(n) + 1;
+                int col = StdRandom.uniform(n) + 1;
+                perc.open(row, col);
+            }
+            results[i] = (double) perc.numberOfOpenSites() / (n * n);
+        }
+    }
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		if (stddev == null) {
-			stddev = StdStats.stddev(results);
-		}
-		return stddev;
-	}
+    // sample mean of percolation threshold
+    public double mean() {
+        if (mean == null) {
+            mean = StdStats.mean(results);
+        }
+        return mean;
+    }
 
-	// low endpoint of 95% confidence interval
-	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(results.length);
-	}
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        if (stddev == null) {
+            stddev = StdStats.stddev(results);
+        }
+        return stddev;
+    }
 
-	// high endpoint of 95% confidence interval
-	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(results.length);
-	}
-	
-	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
-		int trials = Integer.parseInt(args[1]);
-		PercolationStats ps = new PercolationStats(n, trials);
-		System.out.println("Mean: " + ps.mean());
-		System.out.println("Standard deviation: " + ps.stddev());
-		System.out.println("Interval: [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
-	}
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(results.length);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(results.length);
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, trials);
+        System.out.println("Mean: " + ps.mean());
+        System.out.println("Standard deviation: " + ps.stddev());
+        System.out.println("Interval: [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
+    }
 }
