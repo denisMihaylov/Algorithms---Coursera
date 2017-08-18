86189d38703776a9c28d1c1b2f30c586038d7adb
Mihaylov <denis.mihaylov@sap.com>
2017-08-18 18:28:51 +0300

    Move to dedicated package

--- src/programming/queues/Deque.java
+++ src/programming/queues/Deque.java
@@ -1,3 +1,4 @@
+package programming.queues;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
--- src/programming/queues/RandomizedQueue.java
+++ src/programming/queues/RandomizedQueue.java
@@ -1,3 +1,4 @@
+package programming.queues;
 
 import java.util.Arrays;
 import java.util.Iterator;
