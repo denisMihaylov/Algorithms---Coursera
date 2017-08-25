0de990db37ac37437b588c0299c2ea75324312f9
Mihaylov <denis.mihaylov@sap.com>
2017-08-25 19:06:25 +0300

    Finished collinear assingment - Needs improvement

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BruteCollinearPoints {
+
+    private List<LineSegment> segments;
+
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+        Point[] tempPoints = Arrays.copyOf(points, points.length);
+        Arrays.sort(tempPoints);
+        for (int i = 0; i < tempPoints.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) == 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+        computeSegments(tempPoints);
+    }
+
+    public int numberOfSegments() {
+        return segments.size();
+    }
+
+    public LineSegment[] segments() {
+        LineSegment[] result = new LineSegment[segments.size()];
+        return segments.toArray(result);
+    }
+
+    private void computeSegments(Point[] points) {
+        segments = new ArrayList<>();
+        for (int i1 = 0; i1 < points.length - 3; i1++) {
+            for (int i2 = i1 + 1; i2 < points.length; i2++) {
+                if (points[i1].compareTo(points[i2]) == 0) {
+                    throw new IllegalArgumentException();
+                }
+                for (int i3 = i2 + 1; i3 < points.length; i3++) {
+                    for (int i4 = i3 + 1; i4 < points.length; i4++) {
+                        boolean temp1 = points[i1].slopeOrder().compare(points[i2], points[i3]) == 0;
+                        boolean temp2 = points[i1].slopeOrder().compare(points[i3], points[i4]) == 0;
+                        if (temp1 && temp2) {
+                            segments.add(new LineSegment(points[i1], points[i4]));
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+}
---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FastCollinearPoints {
+
+    private List<LineSegment> segments;
+
+    public FastCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+        Point[] tempPoints = Arrays.copyOf(points, points.length);
+        Arrays.sort(tempPoints);
+        for (int i = 0; i < tempPoints.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) == 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+        computeSegments(tempPoints);
+    }
+
+    public int numberOfSegments() {
+        return segments.size();
+    }
+
+    public LineSegment[] segments() {
+        LineSegment[] result = new LineSegment[segments.size()];
+        return segments.toArray(result);
+    }
+    
+    private class SlopeAndNaturalComparator implements Comparator<Point> {
+        
+        private final Point startingPoint;
+        
+        public SlopeAndNaturalComparator(Point startingPoint) {
+            this.startingPoint = startingPoint;
+        }
+
+        @Override
+        public int compare(Point o1, Point o2) {
+            int temp = startingPoint.slopeOrder().compare(o1, o2);
+            if (temp == 0) {
+                return o1.compareTo(o2);
+            }
+            return temp;
+        }
+        
+    }
+
+    private void computeSegments(Point[] points) {
+        segments = new ArrayList<>();
+        Point[] initialPoints = Arrays.copyOf(points, points.length);
+        for (int i = 0; i < initialPoints.length; i++) {
+            Point startingPoint = initialPoints[i];
+            Arrays.sort(points, new SlopeAndNaturalComparator(startingPoint));
+            boolean isAdded = false;
+            int pointCount, j;
+            for (pointCount = 2, j = 1; j < points.length - 1; j++) {
+                if (startingPoint.compareTo(points[j]) == 0 || startingPoint.compareTo(points[j + 1]) == 0) {
+                    continue;
+                }
+                double slope1 = startingPoint.slopeTo(points[j]);
+                double slope2 = startingPoint.slopeTo(points[j + 1]);
+                if (Double.doubleToLongBits(slope1) != Double.doubleToLongBits(slope2)) {
+                    if (pointCount > 3 && !isAdded) {
+                        segments.add(new LineSegment(startingPoint, points[j]));
+                    }
+                    isAdded = false;
+                    pointCount = 2;
+                } else {
+                    pointCount++;
+                    if (startingPoint.compareTo(points[j]) >= 0 || startingPoint.compareTo(points[j + 1]) >= 0) {
+                        isAdded = true;
+                    }
+                }
+            }
+            if (pointCount > 3 && !isAdded) {
+                segments.add(new LineSegment(startingPoint, points[j]));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setPenRadius(0.05);
+        StdDraw.setXscale(0, 25);
+        StdDraw.setYscale(0, 25);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.setPenRadius(0.02);
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+
+}
